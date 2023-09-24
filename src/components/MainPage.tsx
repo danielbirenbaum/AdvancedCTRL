a02f381a2fe02263d4a96947a74d51e8ece2a723
Daniel <dani.b.g.castro@gmail.com>
2023-09-24 11:52:59 -0300

    Mostrando main page na aplicacao e alinhando titulo

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,22 +1,20 @@
 // generalização das páginas principais (tais como a página da CPU , GPU etc)
 interface MainPageProps {
-    name : string , 
-    mainPageContent : Function , 
-
+  name: string;
 }
 
-function MainPage (props : MainPageProps){
-    return <div id ="mainpage">
-        <div id="mainpage-title">
-            {//Espanha por favor não me mata eu não queria fazer isso mas js é muito broxa
-            }
-            <h1>{props.name}</h1>
-        </div>
+function MainPage(props: MainPageProps) {
+  return (
+    <div id="mainpage">
+      <div id="mainpage-title">
+        <h1>{props.name}</h1>
+      </div>
 
-        <hr className="separator"/>
+      <hr className="separator" />
 
-        <div id="mainpage-content">
-            {props.mainPageContent()}
-        </div>
+      <div id="mainpage-content"></div>
     </div>
+  );
 }
+
+export default MainPage;
