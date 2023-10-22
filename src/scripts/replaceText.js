f597d00fe04879900291fc964dae3f3be179f6ba
Daniel <dani.b.g.castro@gmail.com>
2023-10-21 23:16:56 -0300

    Página da memória RAM
    Início da construção da página da memória RAM, falta adequação do CSS e pegar alguns dados

--- src/scripts/replaceText.js
+++ src/scripts/replaceText.js
@@ -5,6 +5,8 @@ console.log(texts);
 // Replace texts
 (async () => {
 	const cpuInfo = await window.app.cpuInfo();
+	const ramInfo = await window.app.ramInfo();
+	const ramTotal = Math.floor((await window.app.ramInfo()).size / 1e9) + "GB";
 
 	for (let i = 0; i < texts.length; i++) {
 		const attr = texts[i].getAttribute("data-info");
@@ -14,6 +16,8 @@ console.log(texts);
 		else if (attr == "cpu-brand") text = cpuInfo.brand;
 		else if (attr == "cpu-base-speed") text = cpuInfo.speed + " GHz";
 		else if (attr == "cpu-cores") text = cpuInfo.cores;
+		else if (attr == "ram-total") text = ramTotal;
+		else if (attr == "ram-type") text = ramInfo.type;
 
 		texts[i].innerHTML = text;
 	}
