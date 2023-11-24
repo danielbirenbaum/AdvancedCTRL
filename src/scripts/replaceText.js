f5fbdf1716efd5d44dcca8d3e82efa8999cd8c14
Davi Golebiovski <davigoleba@gmail.com>
2023-11-24 14:53:10 -0300

    Correção da tela do disco

--- src/scripts/replaceText.js
+++ src/scripts/replaceText.js
@@ -19,11 +19,13 @@ console.log(texts);
 		else if (attr == "cpu-cores") text = cpuInfo.cores;
 		else if (attr == "gpu-vendor") text = gpuInfo.vendor;
 		else if (attr == "gpu-model") text = gpuInfo.model;
-		else if (attr == "ram-total") text = Math.floor(ramInfo.size / 1e9) + "GB";
+		else if (attr == "ram-total")
+			text = Math.floor(ramInfo.size / 1e9) + "GB";
 		else if (attr == "ram-type") text = ramInfo.type;
 		else if (attr == "disk-name") text = diskInfo.name;
 		else if (attr == "disk-type") text = diskInfo.type;
-		else if (attr == "disk-size") text = diskInfo.size;
+		else if (attr == "disk-size")
+			text = Math.floor(diskInfo.size / 1e9) + "GB";
 		else if (attr == "disk-interface") text = diskInfo.interfaceType;
 
 		texts[i].innerHTML = text;
